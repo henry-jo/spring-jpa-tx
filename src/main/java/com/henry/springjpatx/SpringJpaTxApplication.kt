5469d1e5ae35e679596960c81efcca1cf480fc4a
henry <henry@lendit.co.kr>
2019-12-24 21:54:24 +0900

    init project

---
+++ src/main/java/com/henry/springjpatx/SpringJpaTxApplication.kt
@@ -0,0 +1,11 @@
+package com.henry.springjpatx
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringJpaTxApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(SpringJpaTxApplication::class.java, *args)
+}
---
+++ src/test/java/com/henry/springjpatx/SpringJpaTxApplicationTests.java
@@ -0,0 +1,12 @@
+package com.henry.springjpatx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaTxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
